5e5e3fc10778377626076d1c38542905e369dea9
Hirotsune-711 <162408750+Hirotsune-711@users.noreply.github.com>
2024-03-28 18:04:39 +0900

    追加

--- app/routes.py
+++ app/routes.py
@@ -17,3 +17,15 @@ def init_app(app):
     @app.route('/answer')
     def answer():
         return render_template('04_answer.html')  
+    
+    @app.route('/howToUse')
+    def howToUse():
+        return render_template('81_howToUse.html')
+    
+    @app.route('/termsOfUse')
+    def termsOfUse():
+        return render_template('82_termsOfUse.html')
+    
+    @app.route('/aboutThisSite')
+    def aboutThisSite():
+        return render_template('83_aboutThisSite.html')
